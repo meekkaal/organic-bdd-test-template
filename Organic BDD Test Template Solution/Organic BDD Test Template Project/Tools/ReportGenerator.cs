a0f67bb06ad9a8fce98619becac46fcbf4ed312b
Michael Sloan <mikesloan47@gmail.com>
2021-06-05 19:02:49 -0400

    initial html reporter implementation and package updates

--- Organic BDD Test Template Solution/Organic BDD Test Template Project/Tools/ReportGenerator.cs
+++ Organic BDD Test Template Solution/Organic BDD Test Template Project/Tools/ReportGenerator.cs
@@ -1,12 +1,106 @@
-﻿using System;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow;
 
 namespace Organic_BDD_Test_Template_Project.Tools
 {
+    [Binding]
     class ReportGenerator
     {
+        [ThreadStatic]
+        private static ExtentTest featureName;
+        [ThreadStatic]
+        private static ExtentTest scenario;
+        private static ExtentReports extent;
+        public static string ReportPath;
+        private readonly ScenarioContext _scenarioContext;
+        private readonly FeatureContext _featureContext;
+
+        public ReportGenerator(ScenarioContext scenarioContext, FeatureContext featureContext)
+        {
+            _scenarioContext = scenarioContext;
+            _featureContext = featureContext;
+        }
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(AppDomain.CurrentDomain.BaseDirectory + "\\report\\");
+            htmlReporter.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Standard;
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+        }
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
+        {
+
+            featureName = extent.CreateTest<Feature>(featureContext.FeatureInfo.Title, featureContext.FeatureInfo.Description);
+            Console.WriteLine("BeforeFeature");
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            string arguments = "";
+
+            foreach (DictionaryEntry item in _scenarioContext.ScenarioInfo.Arguments)
+            {
+                arguments += $"\n{item.Key} : {item.Value}";
+            }
+
+            scenario = featureName.CreateNode<Scenario>(_scenarioContext.ScenarioInfo.Title + $" [{arguments}]", arguments);
+        }
+        [AfterStep]
+        public void InsertReportingSteps()
+        {
+            var stepType = _scenarioContext.StepContext.StepInfo.StepDefinitionType.ToString();
+            if (_scenarioContext.TestError == null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(_scenarioContext.StepContext.StepInfo.Text);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(_scenarioContext.StepContext.StepInfo.Text);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(_scenarioContext.StepContext.StepInfo.Text);
+                else if (stepType == "And")
+                    scenario.CreateNode<And>(_scenarioContext.StepContext.StepInfo.Text);
+            }
+            else if (_scenarioContext.TestError != null)
+            {
+                if (stepType == "Given")
+                {
+                    scenario.CreateNode<Given>(_scenarioContext.StepContext.StepInfo.Text).Fail(_scenarioContext.TestError.Message);
+                }
+                else if (stepType == "When")
+                {
+                    scenario.CreateNode<When>(_scenarioContext.StepContext.StepInfo.Text).Fail(_scenarioContext.TestError.Message);
+                }
+                else if (stepType == "Then")
+                {
+                    scenario.CreateNode<Then>(_scenarioContext.StepContext.StepInfo.Text).Fail(_scenarioContext.TestError.Message);
+                }
+                else if (stepType == "And")
+                {
+                    scenario.CreateNode<And>(_scenarioContext.StepContext.StepInfo.Text).Fail(_scenarioContext.TestError.Message);
+                }
+            }
+        }
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            Console.WriteLine("AfterScenario");
+        }
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            extent.Flush();
+        }
     }
 }
