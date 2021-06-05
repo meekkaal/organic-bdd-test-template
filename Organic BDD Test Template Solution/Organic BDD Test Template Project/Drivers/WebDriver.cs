61419cb0f070031e7834df82e2e01f9f904b15c0
Michael Sloan <mikesloan47@gmail.com>
2021-06-05 19:22:34 -0400

    add initial feature files, web step definition file, and WebDriver driver

--- Organic BDD Test Template Solution/Organic BDD Test Template Project/Drivers/WebDriver.cs
+++ Organic BDD Test Template Solution/Organic BDD Test Template Project/Drivers/WebDriver.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
 
 namespace Organic_BDD_Test_Template_Project.Drivers
 {
     class WebDriver
     {
+        private IWebDriver _webDriver;
+
+        public WebDriver()
+        {
+        }
+
+
+        public IWebDriver CreateChromeDriver()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            _webDriver = new ChromeDriver();
+            return _webDriver;
+        }
+
+        public IWebDriver CreateFireFoxDriver()
+        {
+            new DriverManager().SetUpDriver(new FirefoxConfig());
+            _webDriver = new FirefoxDriver();
+            return _webDriver;
+        }
+
     }
 }
